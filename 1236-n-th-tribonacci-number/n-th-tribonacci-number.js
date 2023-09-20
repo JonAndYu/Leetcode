ab0bbe7e4a436992874f63fdfa11f544ae87a2af
JonAndYu <64448283+JonAndYu@users.noreply.github.com>
2023-09-20 16:14:22 -0700

    Time: 57 ms (20.72%) | Memory: 41.9 MB (21.58%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function(n) {
+    let arr = [0, 1, 1];
+    for (let i = 3; i <= n; i++) arr.push(arr[i-3] + arr[i-2] + arr[i-1]);
+    return arr[n];
+};
