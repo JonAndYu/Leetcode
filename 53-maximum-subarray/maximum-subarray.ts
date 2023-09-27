14a23a65971c7dab2895c7fe39c9a6227a97f361
JonAndYu <64448283+JonAndYu@users.noreply.github.com>
2023-09-27 10:39:11 -0700

    Time: 91 ms (9.89%) | Memory: 52.4 MB (29.28%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.ts
@@ -0,0 +1,26 @@
+function maxSubArray(nums: number[]): number {
+    let res = Number.NEGATIVE_INFINITY;
+    let curr = 0;
+
+    for (let num of nums) {
+        // if curr array is a negative number
+        if (curr < 0) {
+            curr = 0;
+        }
+        curr = curr + num;
+        res = Math.max(res, curr);
+    }
+
+    return res;
+};
+
+/*
+    [-2,1,-3,4,-1,2,1,-5,4]
+    [-2]
+    [-2, 1] -> [1]
+    [1, -3, 4] -> [4]
+
+    [4, -1, 1] -> 4
+    [4, -1, 2] -> 5
+    [4, -1, 0] -> 4
+*/
