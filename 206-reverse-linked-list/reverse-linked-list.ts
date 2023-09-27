7f08b5df98420c53f2a5508c7c63b9c862eb3743
JonAndYu <64448283+JonAndYu@users.noreply.github.com>
2023-09-26 22:41:41 -0700

    Time: 70 ms (8.41%) | Memory: 44.3 MB (95.27%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.ts
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let prev = null;
+    let curr = head;
+    let next = null;
+
+    while (curr) {
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+};
+
+/*
+    null -> 1 -> 2 -> 3 -> 4 -> 5
+
+    prev ptr = null
+    curr ptr = 1
+    next ptr = null
+
+    curr ptr.next ptr  = prev ptr
+    next ptr.p
+
+*/
