057967d1b6ba5a200f485954434c0e40ddcc62fd
JonAndYu <64448283+JonAndYu@users.noreply.github.com>
2023-10-04 11:19:54 -0700

    Time: 64 ms (45.14%) | Memory: 43.2 MB (8.73%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.ts
@@ -0,0 +1,9 @@
+function fib(n: number): number {
+    let fib = Array(n + 1).fill(0);
+    fib[1] = 1;
+
+    for (let i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[n];
+}
