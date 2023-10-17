b9e2b5992db8470f438c7c0f0ce851b08d6a374a
JonAndYu <64448283+JonAndYu@users.noreply.github.com>
2023-10-16 17:19:24 -0700

    Time: 69 ms (34.56%) | Memory: 44.6 MB (67.96%) - LeetSync

---
+++ 139-word-break/word-break.ts
@@ -0,0 +1,22 @@
+function wordBreak(s: string, wordDict: string[]): boolean {
+    wordDict.sort((a, b) => b.length - a.length);
+
+    const memo: Record<string, boolean> = {};
+    
+    function backtrack(s: string): boolean {
+        if (!s || s.length === 0) return true;
+        if (s in memo) return memo[s];
+
+        let ans = false;
+
+        for (const word of wordDict) {
+            if (s.startsWith(word)) {
+                ans = backtrack(s.substring(word.length))
+                memo[s] = ans;
+                if (ans) break;
+            }
+        }
+        return ans;
+    }
+    return backtrack(s);
+};
