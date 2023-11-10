c9ff19b4f76ed268ebca00d330d69ccf5ad17ff5
JonAndYu <64448283+JonAndYu@users.noreply.github.com>
2023-11-09 21:29:16 -0800

    Time: 91 ms (57.47%) | Memory: 55.1 MB (43.27%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.ts
+++ 238-product-of-array-except-self/product-of-array-except-self.ts
@@ -1,26 +1,21 @@
 function productExceptSelf(nums: number[]): number[] {
-    let output = new Array<number>(nums.length);
+    const n = nums.length;
+    const output = new Array<number>(n);
+    let totalProduct = 1;
 
-    // Set the prefix of the first index
-    output[0] = 1;
-
-    for (let i = 1; i < nums.length; i++) {
-        output[i] = output[i-1] * nums[i - 1];
+    // Calculate the product of all elements to the left of each element
+    for (let i = 0; i < n; i++) {
+        output[i] = totalProduct;
+        totalProduct *= nums[i];
     }
 
-    let postfix = 1;
+    totalProduct = 1; // Reset the total product
 
-    for (let i = nums.length - 1; i >= 0; i--) {
-        output[i] *= postfix;
-        postfix *= nums[i];
+    // Calculate the product of all elements to the right of each element
+    for (let i = n - 1; i >= 0; i--) {
+        output[i] *= totalProduct;
+        totalProduct *= nums[i];
     }
 
     return output;
-};
-
-/*
-    [1,2,3,4]
-
-    output array [1, 1, 2, 6]
-    postfix array [24, 12, 4, 1]
-*/
+}
