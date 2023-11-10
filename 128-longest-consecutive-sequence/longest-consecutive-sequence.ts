255198fcf27622a664bb80aa4eb8f7b60143229d
JonAndYu <64448283+JonAndYu@users.noreply.github.com>
2023-11-09 21:37:34 -0800

    Time: 1334 ms (24.29%) | Memory: 64.1 MB (56.69%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.ts
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.ts
@@ -4,14 +4,13 @@ function longestConsecutive(nums: number[]): number {
 
     for (let i = 0; i < nums.length; i++) {
         let num = nums[i];
-        let currSeq = -1;
 
         // Start of a sequence
         if (!set.has(num - 1)) {
-            currSeq = 1;
+            let currSeq = 1;
             while (set.has(++num)) currSeq++;
+            count = Math.max(count, currSeq)
         }
-        count = Math.max(count, currSeq)
     }
 
     return count;
