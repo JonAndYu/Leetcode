a22b4bf9c9cc1ca53f0666699f45553fc010d7b8
JonAndYu <64448283+JonAndYu@users.noreply.github.com>
2023-11-09 20:51:28 -0800

    Time: 69 ms (76.89%) | Memory: 46.3 MB (41.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -1,1 +1,11 @@
-var isAnagram = (s, t) => s.split('').sort().join('') === t.split('').sort().join('') ? true : false
+function isAnagram(s: string, t: string): boolean {
+    if (s.length !== t.length) return false;
+
+    let sFreqMap = s.split('').reduce((acc, v) => (acc[v] = (acc[v] ?? 0) + 1, acc), {});
+    let tFreqMap = t.split('').reduce((acc, v) => (acc[v] = (acc[v] ?? 0) + 1, acc), {});
+
+    for (let char in sFreqMap) {
+        if (sFreqMap[char] !== tFreqMap[char]) return false;
+    }
+    return true;
+};
