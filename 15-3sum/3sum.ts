c14d58c3e3ea191ef95869fc653c78b91815e37c
JonAndYu <64448283+JonAndYu@users.noreply.github.com>
2023-11-09 22:57:24 -0800

    Time: 176 ms (37.80%) | Memory: 58.4 MB (92.60%) - LeetSync

---
+++ 15-3sum/3sum.ts
@@ -0,0 +1,34 @@
+function threeSum(nums: number[]): number[][] {
+    nums.sort((a,b) => a-b);
+    let n = nums.length;
+
+    const triplets = [];
+
+    for (let i = 0; i < n; i++) {
+        while (i > 0 && nums[i] === nums[i-1]) i++;
+        let firstNum = nums[i];
+
+        let left = i + 1;
+        let right = n - 1;
+
+        while (left < right) {
+            let secondNum = nums[left];
+            let thirdNum = nums[right];
+
+            let sum = firstNum + secondNum + thirdNum
+
+            if (sum === 0) {
+                triplets.push([firstNum, secondNum, thirdNum]);
+                while (left < right && nums[left] === nums[left+1]) left++;
+                while (left < right && nums[right] === nums[right - 1]) right--;
+                left++;
+                right--;
+            } else if (sum > 0) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+    }
+    return triplets;
+};
