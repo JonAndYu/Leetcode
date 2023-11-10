a8d425c5f56c44714545bf3a733d201c58bc2430
JonAndYu <64448283+JonAndYu@users.noreply.github.com>
2023-11-09 21:17:11 -0800

    Time: 115 ms (51.04%) | Memory: 53.2 MB (77.39%) - LeetSync

--- 49-group-anagrams/group-anagrams.ts
+++ 49-group-anagrams/group-anagrams.ts
@@ -1,55 +1,15 @@
 function groupAnagrams(strs: string[]): string[][] {
-    /*
-        Time Complexity: O(n)
-        Space Complexity: O(n)
+    const anagramGroups: Map<string, string[]> = new Map();
 
-        Checks if two strings are anagrams of eachother
-    */
-    function isAnagram(s: string, t: string) {
-        if (s.length !== t.length) return false;
+    for (let str of strs) {
+        let sortedStr = str.split('').sort().join("");
 
-        let sFreq = s.split('').reduce((acc, v) => (acc[v] = (acc[v] ?? 0) + 1, acc), {});
-
-        for (let c of t) {
-            if (sFreq.hasOwnProperty(c) && sFreq[c] > 0) {
-                sFreq[c]--;
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /*
-        Time Complexity: O(nlogn)
-        Space Complexity: O(1)
-
-        Sorts a list of strings by length in ascending order.
-    */
-    function sortStrings(strs: string[]): string[] {
-        return strs.sort((a, b) => a.length - b.length);
-    }
-
-    strs = sortStrings(strs);
-
-    const groups: string[][] = [];
-    const visited: boolean[] = new Array(strs.length).fill(false);
-
-    for (let i = 0; i < strs.length; i++) {
-        if (!visited[i]) {
-            const group:string[] = [strs[i]];
-
-            for (let j = i + 1; j < strs.length; j++) {
-                if (strs[j].length > strs[i].length) break;
-                if (!visited[j] && isAnagram(strs[i], strs[j])) {
-                    group.push(strs[j]);
-                    visited[j] = true;
-                }
-            }
-
-            groups.push(group);
+        if (anagramGroups.has(sortedStr)) {
+            anagramGroups.get(sortedStr).push(str);
+        } else {
+            anagramGroups.set(sortedStr, [str]);
         }
     }
-    return groups;
+    return Array.from(anagramGroups.values());
 };
 
